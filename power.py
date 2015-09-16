f18bd60e79045ee2a31f12ff3f9d554d3ec945f4
Anupama Rajaram <anupamaprv@users.noreply.github.com>
2015-09-16 09:59:04 -0400

    Create power.py

---
+++ power.py
@@ -0,0 +1,10 @@
+# Function to calculate power of a number 
+# Arguments = base, exponent
+
+# Function definition
+def power(base, exponent):  # Add your parameters here!
+    result = base**exponent
+    print "%d to the power of %d is %d." % (base, exponent, result)
+
+# Function call
+power(37,4)  # Add your arguments here!
